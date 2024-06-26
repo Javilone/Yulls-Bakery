11716ecde2f8a41bf53860a6fc4faa506c20ba28
Javilone <javilone.blummer@gmail.com>
2024-06-26 14:56:48 +0200

    Cambios de estilos

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,13 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+
+
+// MAIN CAROUSEL
 const myCarouselElement = document.querySelector('#carouselFade')
 const carousel = new bootstrap.Carousel(myCarouselElement, {
     interval: 6000,
     touch: true
 })
+
+// PRICE PRODUCT HIDDEN - REVEAL
