b71a35d361eb4b502821dc8b37d5c0edac1e9208
Javilone <javilone.blummer@gmail.com>
2024-07-12 12:49:11 +0200

    Arreglo submit. Sección Información añadida

--- src/js/formHandles.js
+++ src/js/formHandles.js
@@ -1,22 +1,36 @@
-$(document).ready(function() {
-    $('#mensaje').addClass('form-message-window'); // Agregar la clase inicialmente
-    $('#mensaje').hide(); // Ocultar el mensaje inicialmente
+// FORM RESPONSE MANAGEMENT 
+const messageWindow = document.getElementById("mensaje");
+
+function messageWindowFunction() {
+    messageWindow.classList.toggle('form-message-window');
+}
+
+messageWindow.addEventListener("click", messageWindowFunction);
+
+
+// FORM AJAX MANAGEMENT 
+
+const submitBtn = document.getElementById("submitBuyBtn");
+
+$(document).ready(function () {
+    $('#mensaje').addClass('form-message-window');
+    $('#mensaje').hide(); // Oculto el contenedor con el id mensaje
 
     $('#yullsForm').submit(function(e) {
-        e.preventDefault(); // Evita que el formulario se envíe normalmente
+        e.preventDefault(); // Evito que el formulario se envíe
 
-        
-        $('#mensaje').show(); // Muestra el contenedor mensaje antes de enviar la solicitud AJAX
+        $('#mensaje').show(); // Muestro el contenedor mensaje antes de enviar la solicitud AJAX
 
-        // Envía el formulario usando AJAX
+        // Envío el formulario usando AJAX
         $.ajax({
             type: 'POST',
             url: 'https://javilonedev.es/index.php',
-            data: $(this).serialize(), // Serializa los datos del formulario
+            data: $(this).serialize(), // Serializo los datos del formulario
             success: function(response) {
-                // Aquí se maneja la respuesta del servidor
+                // Manejo de la respuesta del servidor
                 if (response.trim() === 'enviado') {
                     $('#mensaje').html('<p>Mensaje enviado correctamente.</p>');
+                    submitBtn.disabled = true; // Deshabilitar el botón directamente
                 } else {
                     $('#mensaje').html('<p>Hubo un problema al enviar el mensaje.</p>');
                 }
--- src/js/main.js
+++ src/js/main.js
@@ -19,25 +19,16 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 
-// FORM RESPONSE MANAGEMENT 
-const messageWindow = document.getElementById("mensaje");
-
-function messageWindowFunction() {
-    messageWindow.classList.toggle('form-message-window');
-}
-
-messageWindow.addEventListener("click", messageWindowFunction);
-
 // BUTTON TO GO UP 
 const goUpBtn = document.querySelector('.goUp-btn');
 const upBtnInitialPos = goUpBtn.offsetTop + 250;
 
 // Función para verificar la posición de scroll
 function verificarScroll() {
-    // Obtiene la posición actual de scroll
+    // Obtengo la posición actual de scroll
     const scrollY = window.scrollY || window.pageYOffset;
 
-    // Verifica si se ha hecho scroll hacia abajo más allá de la posición inicial
+    // Verifico si se ha hecho scroll hacia abajo más allá de la posición inicial
     if (scrollY > upBtnInitialPos) {
         goUpBtn.classList.add('fixed');
     } else {
