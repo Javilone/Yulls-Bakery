56f4ce66e8b9fb86c257ab22204bf32bb506afcd
Javilone <javilone.blummer@gmail.com>
2024-09-18 10:26:15 +0200

    Agregado formulario de ejemplo de la tarta floral

--- src/formhandler.php
+++ src/formhandler.php
@@ -5,11 +5,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Capturar y limpiar los datos del formulario
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone = trim($_POST["phone"]);
+    $gluten = trim($_POST["gluten"]);
     $message = trim($_POST["message"]);
     $order = trim($_POST["order"]);
+    $cake = trim($_POST["cake"]);
+    $frosting = trim($_POST["frosting"]);
+    $portions = trim($_POST["portions"]);
 
     // Validar los datos del formulario
-    if (empty($name) || empty($order) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (empty($name) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         http_response_code(400);
         echo "Por favor, complete el formulario correctamente.";
         exit;
@@ -17,13 +22,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Dirección de correo del destinatario y asunto del correo
     $recipient = "contacto@javilonedev.es";
-    $subject = "Yull's Bakery - Nuevo mensaje de $name";
+    $subject = "Yull's Bakery - Nuevo mensaje de $email";
 
     // Contenido del correo electrónico
-    $email_content = "Nombre: $name\n";
-    $email_content .= "Email: $email\n";
-    $email_content .= "Pedido: $order\n";
-    $email_content .= "Mensaje:\n$message\n";
+    $email_content = "NOMBRE: $name\n";
+    $email_content .= "EMAIL: $email\n";
+    $email_content .= "TELÉFONO: $phone\n";
+    $email_content .= "GLUTEN: $gluten\n";
+    $email_content .= "PEDIDO: $order\n";
+    $email_content .= "TIPO DE BIZCOCHO: $cake\n";
+    $email_content .= "SABOR DE FROSTING: $frosting\n";
+    $email_content .= "PORCIONES: $portions\n";
+    $email_content .= "MENSAJE:\n$message\n";
 
     // Cabeceras del correo
     $email_headers = "From: $name <$email>";
--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,38 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
-import './productsButtons.js';
-import './formHandles.js';
-
+import "./productsButtons.js";
+import "./formHandles.js";
 
 // MAIN CAROUSEL
-const myCarouselElement = document.querySelector('#carouselFade')
+const myCarouselElement = document.querySelector("#carouselFade");
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-    interval: 6000,
-    touch: true
-})
+  interval: 6000,
+  touch: true,
+});
 
 // BOOTSTRAP DEPENDENCIES FOR POPOVER (EMERGENT POPUPS INFO)
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
 
-
-// BUTTON TO GO UP 
-const goUpBtn = document.querySelector('.goUp-btn');
+// BUTTON TO GO UP
+const goUpBtn = document.querySelector(".goUp-btn");
 const upBtnInitialPos = goUpBtn.offsetTop + 250;
 
 // Función para verificar la posición de scroll
 function verificarScroll() {
-    // Obtengo la posición actual de scroll
-    const scrollY = window.scrollY || window.pageYOffset;
+  // Obtengo la posición actual de scroll
+  const scrollY = window.scrollY || window.pageYOffset;
 
-    // Verifico si se ha hecho scroll hacia abajo más allá de la posición inicial
-    if (scrollY > upBtnInitialPos) {
-        goUpBtn.classList.add('fixed');
-    } else {
-        goUpBtn.classList.remove('fixed');
-    }
+  // Verifico si se ha hecho scroll hacia abajo más allá de la posición inicial
+  if (scrollY > upBtnInitialPos) {
+    goUpBtn.classList.add("fixed");
+  } else {
+    goUpBtn.classList.remove("fixed");
+  }
 }
 
-window.addEventListener('scroll', verificarScroll);
+document.addEventListener("scroll", verificarScroll);
