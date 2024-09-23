d8af37041ef2581750e8c907f66935187fa2049c
Javilone <javilone.blummer@gmail.com>
2024-09-23 11:09:48 +0200

    Prueba de build con html

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ export default {
   root: resolve(__dirname, "src"),
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        tartafloral: resolve(__dirname, "src/html/tartafloral.html"),
+      },
+    },
   },
   server: {
     port: 8080,
